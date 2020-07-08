40eb229d7ac7c8de4a5016468025ef37ed6f0026
Bridget Almas <balmas@users.noreply.github.com>
2020-07-08 05:52:58 -0400

    fix alpheios.js

--- js/alpheios.js
+++ js/alpheios.js
@@ -1,21 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event){
-
-  import ("https://cdn.jsdeliver.net/npm/alpheios-embedded@latest/dist/alpheios-embedded.min.js")
-  .then(embedLib => {
-
+document.addEventListener("DOMContentLoaded", function(event) {
+  import ("https://cdn.jsdelivr.net/npm/alpheios-embedded@latest/dist/alpheios-embedded.min.js").then(embedLib => {
     window.AlpheiosEmbed.importDependencies({
       mode: 'cdn'
-    })
-    .then(Embedded => {
-
+    }).then(Embedded => {
       new Embedded({
-        clientId: "balmas.github.io/Mikrotata"
+        clientId: 'yoursite.github.io'
       }).activate();
-
+    }).catch(e => {
+      console.error(`Import of Alpheios embedded library dependencies failed: ${e}`)
+    })
+  }).catch(e => {
+    console.error(`Import of Alpheios Embedded library failed: ${e}`)
   })
-  .catch(e => {
-      console.error('Import of Alpheios Embedded library failed: ${e}')
-  })
-
-  })
-})
+});
